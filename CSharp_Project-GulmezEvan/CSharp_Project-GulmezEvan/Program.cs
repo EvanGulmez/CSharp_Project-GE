7f3e3427b5b5674ab43fb5901c016a3ec0e9e584
Luka Gulmez <you@example.com>
2022-12-05 15:03:27 +0100

    TD5 Collection

--- CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
+++ CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
@@ -1,21 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections;
 
-int[] monTbleau; // declaration 
-monTableau = new int[50]; // initialisation
+//ArrayList
 
-// faire un tableau sans donner une taille
+ArrayList uneCollection = new ArrayList();
 
-int[] monTableauSansLimite = new int[] { 1, 45, 6262 };
+uneCollection.Add(new object());
 
+uneCollection.Insert(0, "TEST");
 
-// tableau rectangulaire
+foreach (object o in uneCollection)
+{
+    if (o.GetType() == typeof(string))
+    {
 
-int[,] tmd = new int[3, 2]; // 3 lignes 2 colonnes
-int[,,] tmmd = new int[5, 3, 2];
-
-// tableau en escalier 
-int[][] temd = new int[2][];
-
-temd[0] = new int[] { 2, 5 };
-temd[1] = new int[] { 2, 5, 12, 21 };
+    }
+}
