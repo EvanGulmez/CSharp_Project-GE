776985bbbf6f66fd3813fd30b0901af244643736
Luka Gulmez <you@example.com>
2022-12-05 14:46:54 +0100

    TD4 Tableau

--- CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
+++ CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
@@ -1,13 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
 
-int i = 4;
+int[] monTbleau; // declaration 
+monTableau = new int[50]; // initialisation
 
-long l = i;
+// faire un tableau sans donner une taille
 
-//Conversion explicite
+int[] monTableauSansLimite = new int[] { 1, 45, 6262 };
 
-int m = (int) l;
 
-short n = 300;
+// tableau rectangulaire
 
-byte o =(byte)n;
+int[,] tmd = new int[3, 2]; // 3 lignes 2 colonnes
+int[,,] tmmd = new int[5, 3, 2];
+
+// tableau en escalier 
+int[][] temd = new int[2][];
+
+temd[0] = new int[] { 2, 5 };
+temd[1] = new int[] { 2, 5, 12, 21 };
