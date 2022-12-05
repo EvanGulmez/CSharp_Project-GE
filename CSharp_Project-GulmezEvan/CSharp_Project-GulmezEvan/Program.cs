024de8e26a53fb3519e6e5ec465955b5e6189712
Luka Gulmez <you@example.com>
2022-12-05 15:21:09 +0100

    TD 6 Acces type

--- CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
+++ CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
@@ -1,18 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Collections;
 
-//ArrayList
-
-ArrayList uneCollection = new ArrayList();
-
-uneCollection.Add(new object());
-
-uneCollection.Insert(0, "TEST");
-
-foreach (object o in uneCollection)
+// les attributs et les modificateurs de classe (niveau d'accès) sont placés a 
+// avant le mot cle classe
+class MaClasse
 {
-    if (o.GetType() == typeof(string))
-    {
+    // les membres : méthodes, propriétés
 
-    }
+    public int MyProperty { get; set; }
 }
+// Public
+// Autorise l'accès a tous les types et a tout le projet
+
+// Private
+// Autorise l'accès uniquement pour les autres membres du type
+
+// Internal
+// Uniquement au sein de l'espace de nom du type 
+
+// Protected
+// Accès pour les autres membres du type et pour tous les types héritant
+// de celui-ci même en dehors de l'espace de nom l'"assembly"
