660e90e1cb169db094a3493ba2edc529160021df
Luka Gulmez <you@example.com>
2022-12-06 11:26:11 +0100

    RPS Game ComputerChoice

--- CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
+++ CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
@@ -9,14 +9,37 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Random random= new Random();
+            // Create a random number generator to use for the computer's choices   
+            Random random = new Random();
 
-            int Player = 0;
-            int Computer = 0;
+            // Counter of the number of wins for the player and computer
+            int PlayerWins = 0;
+            int ComputerWins = 0;
 
+            // Play only 3 rounds
             for (int i = 0; i < 3; i++) 
             {
-                int Choice = random.Next(1, 4);
+                // Let the player to enter their choice
+                Console.WriteLine("Enter your choice (Rock, Paper, or Scissors):");
+                // Pass the string in Lowercase 
+                string PlayerChoice = Console.ReadLine().ToLower();
+                // Add the random choice
+                int ComputerChoiceNum = random.Next(1, 4);
+                string ComputerChoice = "";
+
+                if (ComputerChoiceNum == 1)
+                {
+                    ComputerChoice = "rock";
+                }
+                else if (ComputerChoiceNum == 2)
+                {
+                    ComputerChoice = "paper";
+                }
+                else
+                {
+                    ComputerChoice = "scissors";
+                }
+
             }
         }
     }
