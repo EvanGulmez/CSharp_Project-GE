b3b5225312148549a5e5b7d30483c7673121c164
Luka Gulmez <you@example.com>
2022-12-06 11:08:59 +0100

    RPS Game Loop For

--- CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
+++ CSharp_Project-GulmezEvan/CSharp_Project-GulmezEvan/Program.cs
@@ -9,7 +9,15 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-          
+            Random random= new Random();
+
+            int Player = 0;
+            int Computer = 0;
+
+            for (int i = 0; i < 3; i++) 
+            {
+                int Choice = random.Next(1, 4);
+            }
         }
     }
 }
